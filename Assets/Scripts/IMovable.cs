060e5d482d36344b2cffe35f1511dd7805ca2bc2
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-10-21 23:10:06 +0100

    IMovable, ISpawnable interface, changes to plataform, havent tried IMovable

---
+++ Assets/Scripts/IMovable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMovable
+{
+    // Move amount is in increments of 2 (1 square size in unity units)
+    void Move(int moveAmount = 1);
+}
--- Assets/Scripts/Plataform.cs
+++ Assets/Scripts/Plataform.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class Plataform : MonoBehaviour
+public class Plataform : MonoBehaviour, ISpawnable, IMovable
 {
+    [SerializeField] private GameObject[] _enemiesPrefab;
+    [SerializeField] [Range(0, 1f)] private float _speed;
+
     private Animator _anim;
     private bool _hasPlayerBeen;
+    private GameObject _spawnedEnemy;
 
     public bool HasPlayer
     {
@@ -23,8 +27,6 @@ public class Plataform : MonoBehaviour
     {
         _anim = GetComponent<Animator>();
         _hasPlayerBeen = false;
-
-        _anim.SetBool("Die", true);
     }
 
     private void FixedUpdate()
@@ -52,4 +54,23 @@ public class Plataform : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
+    public void Spawn()
+    {
+        _spawnedEnemy = Instantiate(_enemiesPrefab[Random.Range(0, _enemiesPrefab.Length - 1)]
+            , Vector3.zero, transform.rotation);
+
+        _spawnedEnemy.transform.SetParent(gameObject.transform);
+    }
+
+    public void Move(int moveAmount = 1)
+    {
+        Vector3 newPosition;
+
+        newPosition = transform.position;
+        newPosition.z = -2 * moveAmount;
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, _speed);
+    }
+
 }
