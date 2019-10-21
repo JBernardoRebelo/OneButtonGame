5499c4379d627cbe273a506de57284fb25069b30
MizuRyujin <miguelrf95@gmail.com>
2019-10-21 14:14:06 +0100

    Add map script

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    // Class vars
+    [SerializeField] private Plataform blockLarge;
+    private Queue<Plataform> world;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        world = new Queue<Plataform>(5);
+
+        for (int i = 0; i < world.Count; i++)
+        {
+            world.Enqueue(Instantiate(blockLarge));
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
